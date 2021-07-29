ce4c652935fea84b90d3ad9899c4616fb120da07
Karolina Michalak <karolina0michalak@gmail.com>
2021-07-29 08:21:29 +0200

    Using markdown and transformer plugins to list data

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,14 +1,44 @@
 import React from "react"
-import Layout from '../../components/Layout'
-import * as styles from '../../styles/projects.module.css'
+import Layout from "../../components/Layout"
+import * as styles from "../../styles/projects.module.css"
+import { graphql } from "gatsby"
+import { Link } from "@reach/router"
 
-export default function Projects() {
+export default function Projects({ data }) {
+  console.log(data)
+  const projects = data.allMarkdownRemark.nodes
   return (
     <Layout>
       <div className={styles.portfolio}>
         <h2>Portfolio</h2>
         <h3>What I've created</h3>
+        <div>
+          {projects.map(project => (
+            <Link to={"/projects/" + project.frontmatter.slug} key={project.id}>
+              <div>
+                <h3>{project.frontmatter.title}</h3>
+                <p>{project.frontmatter.stack}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </Layout>
   )
 }
+
+// exports page query
+export const query = graphql`
+  query ProjectsPage {
+    allMarkdownRemark {
+      nodes {
+        frontmatter {
+          slug
+          stack
+          title
+        }
+        id
+      }
+    }
+  }
+`
